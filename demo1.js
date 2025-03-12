57de33b160b7a89469eda117ad416900ec05f046
sangilish <56685007+sangilish@users.noreply.github.com>
2025-03-11 23:47:04 -0700

    w2-d1-inclass

--- demo1.js
+++ demo1.js
@@ -1,27 +1,38 @@
 class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-      
-      //add handle
-      
-    }
-    
-    // add handleChange here
-    
-    // add handleSubmit here
-    
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          
-        </form>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
-  ReactDOM.render(
-    <NameForm />,
-    document.getElementById('root')
-  );
+
+  // handleChange 메서드
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  // handleSubmit 메서드 (클래스 내부로 이동)
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+// ReactDOM을 이용해 렌더링
+ReactDOM.render(<NameForm />, document.getElementById('root'));
--- demo3.js
+++ demo3.js
@@ -1,27 +1,28 @@
-  class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      
-    }
-  
-    handleSubmit(event) {
-      
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text"  />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
   }
-  
-  ReactDOM.render(
-    <NameForm />,
-    document.getElementById('root')
-  );
+
+  // handleSubmit 메서드 (클래스 내부로 이동)
+  handleSubmit(event) {
+    event.preventDefault();
+    alert('A name was submitted: ' + this.input.current.value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" ref={this.input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+// ReactDOM을 이용해 렌더링
+ReactDOM.render(<NameForm />, document.getElementById('root'));
